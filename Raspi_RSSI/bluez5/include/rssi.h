d4f0c46407b1ca7f0aa23d3c17598ce4c3d93ddd
Sarthak Ahuja <sarthakahuja11@users.noreply.github.com>
2022-03-23 15:00:06 +0000

    updating another approach
    make cmake folder

---
+++ Raspi_RSSI/bluez5/include/rssi.h
@@ -0,0 +1,24 @@
+#ifndef RSS_H_
+#define RSS_H_
+
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/hci_lib.h>
+#include <stdint.h>
+
+struct sockaddr_l2 {
+  sa_family_t l2_family;
+  unsigned short l2_psm;
+  bdaddr_t l2_bdaddr;
+  unsigned short l2_cid;
+  uint8_t l2_bdaddr_type;
+};
+
+#define L2CAP_CONNINFO 0x02
+struct l2cap_conninfo {
+  uint16_t hci_handle;
+  uint8_t dev_class[3];
+};
+#define ATT_CID 4
+
+#endif /* RSS_H_ */
