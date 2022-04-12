8817cf6282221fd3c03aff2d279a37fda2b761a7
AzvenusK <azvenuskestel19@outlook.com>
2022-04-12 13:49:04 +0100

    "Utils parth-gist"

---
+++ Raspi_RSSI/rssi_parth_gist/build/CMakeFiles/rssi_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rssi_1.dir/src/rssi_1.c.o"
+  "rssi_1"
+  "rssi_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/rssi_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
